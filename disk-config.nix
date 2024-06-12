53def2d3b2a302c74986c414820e997ca48981bd
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-06-12 07:35:57 +0500

    root first, then rest

--- disk-config.nix
+++ disk-config.nix
@@ -1,7 +1,7 @@
 {
   disko.devices = {
     disk = {
-      one = {
+      nvme0n1 = {
         type = "disk";
         device = "/dev/nvme0n1";
         content = {
@@ -15,6 +15,7 @@
             ESP = {
               size = "500M";
               type = "EF00";
+              priority = 2;
               content = {
                 type = "mdraid";
                 name = "boot";
@@ -24,20 +25,20 @@
               size = "32G";
               content = {
                 type = "mdraid";
-                name = "plainSwap";
+                name = "swap";
               };
             };
-            mdadm = {
+            ROOT = {
               size = "100%";
               content = {
                 type = "mdraid";
-                name = "raid1";
+                name = "root";
               };
             };
           };
         };
       };
-      two = {
+      nvme1n1 = {
         type = "disk";
         device = "/dev/nvme1n1";
         content = {
@@ -59,14 +60,14 @@
               size = "32G";
               content = {
                 type = "mdraid";
-                name = "plainSwap";
+                name = "swap";
               };
             };
-            mdadm = {
+            ROOT = {
               size = "100%";
               content = {
                 type = "mdraid";
-                name = "raid1";
+                name = "root";
               };
             };
           };
@@ -74,24 +75,7 @@
       };
     };
     mdadm = {
-      boot = {
-        type = "mdadm";
-        level = 1;
-        metadata = "1.0";
-        content = {
-          type = "filesystem";
-          format = "vfat";
-          mountpoint = "/boot";
-        };
-      };
-      plainSwap = {
-        type = "mdadm";
-        level = 1;
-        content = {
-          type = "swap";
-        };
-      };
-      raid1 = {
+      root = {
         type = "mdadm";
         level = 1;
         content = {
@@ -104,6 +88,23 @@
               mountpoint = "/";
             };
           };
+        };
+      };
+      boot = {
+        type = "mdadm";
+        level = 1;
+        metadata = "1.0";
+        content = {
+          type = "filesystem";
+          format = "vfat";
+          mountpoint = "/boot";
+        };
+      };
+      swap = {
+        type = "mdadm";
+        level = 1;
+        content = {
+          type = "swap";
         };
       };
     };
