9de9a35d1549017e2544d3cfae85e76f9cc562ee
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-06-18 21:04:08 +0500

    Revert "Update disk-config.nix"
    This reverts commit 6979be2400771fb7547e56dedd617cabb939c83c.

--- disk-config.nix
+++ disk-config.nix
@@ -1,20 +1,46 @@
-# /dev/nvme0n1
-# /dev/nvme1n1
-#
-# Disk
-# |- BOOT
-# |- ESP
-# |  |- mdraid
-# |- SWAP
-# |  |- mdraid
-# |- ROOT
-#    |- mdraid
 {
   disko.devices = {
     disk = {
-      one = {
+      nvme0n1 = {
         type = "disk";
         device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            BOOT = {
+              size = "1M";
+              type = "EF02";
+              priority = 1;
+            };
+            ESP = {
+              size = "500M";
+              type = "EF00";
+              priority = 2;
+              content = {
+                type = "mdraid";
+                name = "boot";
+              };
+            };
+            SWAP = {
+              size = "32G";
+              content = {
+                type = "mdraid";
+                name = "swap";
+              };
+            };
+            ROOT = {
+              size = "100%";
+              content = {
+                type = "mdraid";
+                name = "root";
+              };
+            };
+          };
+        };
+      };
+      nvme1n1 = {
+        type = "disk";
+        device = "/dev/nvme1n1";
         content = {
           type = "gpt";
           partitions = {
@@ -37,47 +63,11 @@
                 name = "swap";
               };
             };
-            mdadm = {
+            ROOT = {
               size = "100%";
               content = {
                 type = "mdraid";
-                name = "raid1";
-              };
-            };
-          };
-        };
-      };
-      two = {
-        type = "disk";
-        device = "/dev/nvme1n1";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02";
-              priority = 1; # Needs to be first partition
-            };
-            ESP = {
-              size = "500M";
-              type = "EF00";
-              content = {
-                type = "mdraid";
-                name = "boot";
-              };
-            };
-            SWAP = {
-              size = "32G";
-              content = {
-                type = "mdraid";
-                name = "swap";
-              };
-            };
-            mdadm = {
-              size = "100%";
-              content = {
-                type = "mdraid";
-                name = "raid1";
+                name = "root";
               };
             };
           };
@@ -85,6 +75,21 @@
       };
     };
     mdadm = {
+      root = {
+        type = "mdadm";
+        level = 1;
+        content = {
+          type = "gpt";
+          partitions.primary = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
       boot = {
         type = "mdadm";
         level = 1;
@@ -100,21 +105,6 @@
         level = 1;
         content = {
           type = "swap";
-        };
-      };
-      raid1 = {
-        type = "mdadm";
-        level = 1;
-        content = {
-          type = "gpt";
-          partitions.primary = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
-            };
-          };
         };
       };
     };
